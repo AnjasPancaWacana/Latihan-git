0ec837ea33df886110f21598a5b60770bf31f030
AnjasPancaWacana <wacanaap@gmail.com>
2023-12-21 21:59:33 +0700

    manipulasi String

---
+++ test 2.js
@@ -0,0 +1,13 @@
+
+
+//test 2 manipulasi string
+let NamaDepan = "Anjas Panca"
+let NamaBelakang = "Wacana"
+let gabung = (`Halo nama saya ${NamaDepan} ${NamaBelakang}`)
+console.log(gabung)
+
+let data= "nama saya adit saya sedang belajar javascript"
+let ganti= data.replace( "saya" ,"aku")
+//console.log(ganti);
+
+
